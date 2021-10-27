4f7d4d845422d751b02f84f8bc253352ca3cc655
Cleriston <cleriston.mari@gmail.com>
2021-10-27 12:30:30 +0100

    refactor: remove duplicated code

--- src/controller/basket/add-to-basket-controller.spec.ts
+++ src/controller/basket/add-to-basket-controller.spec.ts
@@ -2,23 +2,41 @@ import { AddProductToBasketModel } from '@/application/basket/protocols/add-prod
 import { AddToBasketController } from './add-to-basket-controller'
 import { DbAddToBasket } from '@/application/basket/protocols/db-add-to-basket'
 import { HttpRequest } from '../protocols/http'
+import { Controller } from '../protocols/controllet'
 
-class AddToBasket implements DbAddToBasket {
-  async add (product: AddProductToBasketModel): Promise<null> {
-    return Promise.resolve(null)
+type SutTypes = {
+  sut: Controller
+  addToBasket: DbAddToBasket
+}
+
+const makeAddToBasketStub = (): DbAddToBasket => {
+  class AddToBasket implements DbAddToBasket {
+    async add (product: AddProductToBasketModel): Promise<null> {
+      return Promise.resolve(null)
+    }
   }
+  return new AddToBasket()
+}
+
+const makeSut = (): SutTypes => {
+  const addToBasket = makeAddToBasketStub()
+  const sut = new AddToBasketController(addToBasket)
+  return {
+    sut,
+    addToBasket
+  }
+}
+
+const addProductToBasketModel: AddProductToBasketModel = {
+  idProduct: 2
+}
+const httpRequest: HttpRequest<AddProductToBasketModel> = {
+  body: addProductToBasketModel
 }
 
 describe('AddToBasket Controller', () => {
   test('Ensure AddToBasketController calls AddToBasket with correct value', () => {
-    const addProductToBasketModel: AddProductToBasketModel = {
-      idProduct: 2
-    }
-    const httpRequest: HttpRequest<AddProductToBasketModel> = {
-      body: addProductToBasketModel
-    }
-    const addToBasket = new AddToBasket()
-    const sut = new AddToBasketController(addToBasket)
+    const { sut, addToBasket } = makeSut()
     const addToBasketSpy = jest.spyOn(addToBasket, 'add')
     sut.handle(httpRequest)
     expect(addToBasketSpy).toBeCalledWith(httpRequest.body)
