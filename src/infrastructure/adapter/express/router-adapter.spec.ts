2f834c7067d9a92bacbea4efcb04ebda3e447147
Cleriston <cleriston.mari@gmail.com>
2021-10-28 01:37:01 +0100

    refactor: create factories on Router Adapter Express

--- src/infrastructure/adapter/express/router-adapter.spec.ts
+++ src/infrastructure/adapter/express/router-adapter.spec.ts
@@ -3,10 +3,9 @@ import { Controller } from '@/controller/protocols/controller'
 import { HttpRequest, HttpResponse } from '@/controller/protocols/http'
 import routerAdapterExpress from './router-adapter'
 
-class AnyControllerStub implements Controller {
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return Promise.resolve(ok())
-  }
+type SutTypes = {
+  sut: any
+  controller: Controller
 }
 
 const fakeExpressRequest = {
@@ -24,12 +23,29 @@ const fakeExpressResponse = {
   send: (message: any): void => { }
 }
 
+const makeControlerStub = (): Controller => {
+  class AnyControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return Promise.resolve(ok())
+    }
+  }
+  return new AnyControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controller = makeControlerStub()
+  const sut = routerAdapterExpress(controller)
+  return {
+    sut,
+    controller
+  }
+}
+
 describe('Router Adapter Express', () => {
   test('Ensure RouterAdapterExpress calls Controller with correct value', async () => {
-    const controller = new AnyControllerStub()
-    const routerAdapter = routerAdapterExpress(controller)
+    const { sut, controller } = makeSut()
     const controllerSpy = jest.spyOn(controller, 'handle')
-    await routerAdapter(fakeExpressRequest, fakeExpressResponse)
+    await sut(fakeExpressRequest, fakeExpressResponse)
     expect(controllerSpy).toBeCalledWith({
       body: {
         idProduct: 1
@@ -38,30 +54,23 @@ describe('Router Adapter Express', () => {
   })
 
   test('Ensure Change Express Response status if controller returns 200', async () => {
-    const controller = new AnyControllerStub()
-    const routerAdapter = routerAdapterExpress(controller)
-    jest.spyOn(controller, 'handle').mockImplementation(async (httpRequest: HttpRequest): Promise<HttpResponse> => {
-      return {
-        statusCode: 200,
-        body: 'success'
-      }
-    })
-    await routerAdapter(fakeExpressRequest, fakeExpressResponse)
+    const { sut, controller } = makeSut()
+    jest.spyOn(controller, 'handle').mockImplementation(async (httpRequest: HttpRequest): Promise<HttpResponse> => ({
+      statusCode: 200,
+      body: 'success'
+    }))
+    await sut(fakeExpressRequest, fakeExpressResponse)
     expect(fakeExpressResponse.status).toBeCalled()
     expect(fakeExpressResponse.status).toBeCalledWith(200)
   })
 
   test('Ensure Change Express Response status if controller returns error', async () => {
-    fakeExpressResponse.status.mockClear()
-    const controller = new AnyControllerStub()
-    const routerAdapter = routerAdapterExpress(controller)
-    jest.spyOn(controller, 'handle').mockImplementation(async (httpRequest: HttpRequest): Promise<HttpResponse> => {
-      return {
-        statusCode: 500,
-        body: serverError('any_error')
-      }
-    })
-    await routerAdapter(fakeExpressRequest, fakeExpressResponse)
+    const { sut, controller } = makeSut()
+    jest.spyOn(controller, 'handle').mockImplementation(async (httpRequest: HttpRequest): Promise<HttpResponse> => ({
+      statusCode: 500,
+      body: serverError('any_error')
+    }))
+    await sut(fakeExpressRequest, fakeExpressResponse)
     expect(fakeExpressResponse.status).toBeCalledWith(500)
   })
 })
